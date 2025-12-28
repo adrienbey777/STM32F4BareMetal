5f3fdddfec50ba0834b43b87c32b8fcda6e30b5d
Adrien_BEY <adrien.bey777@gmail.com>
2025-12-28 21:13:45 +0100

    Add LED driver and BSP implementation

--- drivers/led/src/led.c
+++ drivers/led/src/led.c
@@ -1,37 +1,22 @@
-#include "stm32f4xx.h"  // CMSIS pour STM32F4
 #include "led.h"
+#include "bsp_led.h"
 
-// Initialisation LEDs sur GPIOD
-void led_init(void) {
-    // Activer horloge GPIOD
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-
-    // Configurer les pins en output
-    GPIOD->MODER &= ~((3 << (LED_GREEN*2)) |
-                      (3 << (LED_ORANGE*2)) |
-                      (3 << (LED_RED*2)) |
-                      (3 << (LED_BLUE*2)));
-
-    GPIOD->MODER |=  ((1 << (LED_GREEN*2)) |
-                      (1 << (LED_ORANGE*2)) |
-                      (1 << (LED_RED*2)) |
-                      (1 << (LED_BLUE*2)));
+void led_init(void)
+{
+    bsp_led_init();
 }
 
-// Toggle une LED spécifique
-void led_toggle(uint8_t led)
+void led_on(led_t led)
 {
-    GPIOD->ODR ^= (1 << led);
+    bsp_led_on((uint8_t)led);
 }
 
-// Allumer une LED spécifique
-void led_on(uint8_t led)
+void led_off(led_t led)
 {
-    GPIOD->ODR |= (1 << led);
+    bsp_led_off((uint8_t)led);
 }
 
-// Éteindre une LED spécifique
-void led_off(uint8_t led)
+void led_toggle(led_t led)
 {
-    GPIOD->ODR &= ~(1 << led);
+    bsp_led_toggle((uint8_t)led);
 }
